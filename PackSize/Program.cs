9c600a161f96498ad9577fced93cbd0c34f6f98d
bigdwilly <derek@8williams.com>
2016-01-28 10:02:47 -0700

    initial Main method and feed into jagged array

--- PackSize/Program.cs
+++ PackSize/Program.cs
@@ -10,6 +10,40 @@ namespace PackSize
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            string[] classArray = { "Intro to Arguing on the Internet: Godwin’s Law", "Understanding Circular Logic: Intro to Arguing on the Internet", "Godwin’s Law: Understanding Circular Logic", "Introduction to Paper Airplanes: ", "Advanced Throwing Techniques: Introduction to Paper Airplanes", "History of Cubicle Siege Engines: Rubber Band Catapults 101", "Advanced Office Warfare: History of Cubicle Siege Engines", "Rubber Band Catapults 101: ", "Paper Jet Engines: Introduction to Paper Airplanes" };
+            Console.WriteLine(p.GetCourseOrder(classArray));
+            Console.Read();
+        }
+
+        public string GetCourseOrder(string[] classArray)
+        {
+            string result = String.Empty;
+            List<string> errors = new List<string>();
+            //use a string list to store the courses
+            List<string> courseList = new List<string>();
+
+            try
+            {
+                //feed into a jagged array
+                string[][] prereqsArray = new string[classArray.Count()][];
+                for (int i = 0; i <= classArray.Count() - 1; i++)
+                {
+                    List<string> subList = classArray[i].Split(new[] { ": " }, StringSplitOptions.None).ToList();
+                    string[] subArray = subList.FindAll(x => x.Trim() != String.Empty).ToArray();
+                    prereqsArray[i] = subArray;
+                }
+            }
+            catch (Exception exc)
+            {
+                errors.Add(exc.InnerException.ToString());
+            }
+
+            if (errors.Count > 0)
+            {
+                result = String.Concat(result, ". ERROR(s): ", String.Join(", ", errors));
+            }
+            return result;
         }
     }
 }
